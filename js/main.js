425b80afb5ede2f3f51ce96321f02a763577f8bf
Chen Ozer <chen572@gmail.com>
2020-06-20 10:25:36 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const cart = [];
+const products = [
+    {name: 'Harry Potter and the Chamber of Secrets', price: 10, img: 'https://images-na.ssl-images-amazon.com/images/I/51uDvYtlmwL.jpg'},
+    {name: 'Jaws', price: 20, img: 'https://images-na.ssl-images-amazon.com/images/I/71U4vaOry-L._SX522_.jpg'},
+    {name: 'Lord of the Rings: The Two Towers', price: 15, img: 'https://images-na.ssl-images-amazon.com/images/I/915my3fk4QL._SX522_.jpg'},
+    {name: 'Star Wars: The Rise of Skywalker', price: 18, img: 'https://images-na.ssl-images-amazon.com/images/I/819S9CGB5nL._SX522_.jpg'},
+    {name: 'E.T. The Extra-Terrestrial', price: 5, img: 'https://images-na.ssl-images-amazon.com/images/I/71hePU1hACL._SY679_.jpg'},
+    {name: 'The Hobbit: The Desolation of Smaug', price: 10, img: 'https://images-na.ssl-images-amazon.com/images/I/51xfCoIXKNL.jpg'},
+    {name: 'John Wick', price: 12, img: 'https://images-na.ssl-images-amazon.com/images/I/5180Jb6iREL.jpg'},
+    {name: 'Jurassic Park', price: 8, img: 'https://images-na.ssl-images-amazon.com/images/I/81dEScugRUL._SX522_.jpg'}
+];
